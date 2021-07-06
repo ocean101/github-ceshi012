a2ee8e16839495675741e21b082fb9603b0dbcfc
Administrator <910853073@qq.com>
2021-07-06 11:08:16 +0800

    idea上传github测试01

---
+++ src/main/java/com/czxy/GithubCeshi01Application.java
@@ -0,0 +1,13 @@
+package com.czxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCeshi01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCeshi01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/czxy/GithubCeshi01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCeshi01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
